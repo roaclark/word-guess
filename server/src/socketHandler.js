b0e1b83c0fd5ccae1a7a2f7115f6e0b1590a6aee
Rachel Rosalia <rachel@rosalia.me>
2020-05-14 02:19:58 -0700

    Create new word message

--- server/src/socketHandler.js
+++ server/src/socketHandler.js
@@ -10,7 +10,22 @@ import {
 
 const socketToUsers: { [string]: ?string } = {};
 
-const joinRoom = ({ username, room }, socket, server) => {
+const getDetailsForSocket = (
+  socket: Socket,
+): { username: ?string, room: ?string } => {
+  const username = socketToUsers[socket.id];
+  if (!username) {
+    return { username: null, room: null };
+  }
+  const room = getUserRoom(username);
+  return { username, room };
+};
+
+const joinRoom = (
+  { username, room }: { username: string, room: string },
+  socket: Socket,
+  server: Server,
+): void => {
   console.log(`user ${username} joined ${room}`);
   addUserToRoom(username, room);
   socket.join(room, () => {
@@ -21,24 +36,40 @@ const joinRoom = ({ username, room }, socket, server) => {
   socketToUsers[socket.id] = username;
 };
 
-const leaveRoom = (socket, server) => {
-  const username = socketToUsers[socket.id];
-  if (username) {
-    const room = getUserRoom(username);
-    if (room) {
-      console.log(`user ${username} left ${room}`);
-      removeUserFromRoom(username, room);
-      server.sockets
-        .to(room)
-        .emit('user left', { username, users: getUsersInRoom(room) });
-    }
+const leaveRoom = (socket: Socket, server: Server): void => {
+  const { username, room } = getDetailsForSocket(socket);
+  if (username && room) {
+    console.log(`user ${username} left ${room}`);
+    removeUserFromRoom(username, room);
+    server.sockets
+      .to(room)
+      .emit('user left', { username, users: getUsersInRoom(room) });
   }
   delete socketToUsers[socket.id];
+};
+
+const selectRandom = (lis) => {
+  if (lis.length < 1) {
+    return null;
+  }
+  const index = Math.floor(Math.random() * lis.length);
+  return lis[index];
+};
+
+const selectNewWord = (socket: Socket, server: Server): void => {
+  const { room } = getDetailsForSocket(socket);
+  if (room) {
+    const nextUser = selectRandom(getUsersInRoom(room));
+    if (nextUser) {
+      server.sockets.to(room).emit('word', { guesser: nextUser, word: 'foo' });
+    }
+  }
 };
 
 const setup = (server: Server) => (socket: Socket) => {
   socket.on('join room', (e) => joinRoom(e, socket, server));
   socket.on('leave room', () => leaveRoom(socket, server));
+  socket.on('new word', () => selectNewWord(socket, server));
   console.log('a user connected');
   socket.on('disconnect', () => {
     leaveRoom(socket, server);
