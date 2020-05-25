22bb97ec917dfabfb887ca86fcb2cd13b70eb977
Rachel Rosalia <rachel@rosalia.me>
2020-05-24 19:23:12 -0700

    Add categories endpoint to server

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import path from 'path';
 import socketio from 'socket.io';
-import socketHandler from './socketHandler.js';
+import socketHandler from './socketHandler';
+import categories from './categories';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/', express.static(path.join(__dirname, '../../client/dist')));
+
+app.get('/api/categories', (req, res) => {
+  res.json(
+    Object.entries(categories).map(([id, { label }]) => ({
+      id,
+      label,
+    })),
+  );
+});
 
 const server = app.listen(port, () => {
   console.log(`Server listening on port ${port}!`);
