28738fcdcb86495ec7bb12d3d2982c8470731220
Rachel Rosalia <rachel@rosalia.me>
2020-05-24 19:07:16 -0700

    Select styling

--- client/src/Select.jsx
+++ client/src/Select.jsx
@@ -11,23 +11,33 @@ const Select = ({
   onChange?: (string) => void,
 }) => {
   return (
-    <select
-      style={{
-        display: 'inline-block',
-        border: '1px solid #ccc',
-        fontSize: '16px',
-      }}
-      value={value}
-      onChange={(e) => {
-        onChange && onChange(e.target.value);
-      }}
-    >
-      {options.map(({ name, value }) => (
-        <option value={value} key={value}>
-          {name}
-        </option>
-      ))}
-    </select>
+    <div>
+      <label
+        htmlFor="category-select"
+        style={{ display: 'inline-block', paddingRight: '5px' }}
+      >
+        Word list:
+      </label>
+      <select
+        id="category-select"
+        style={{
+          display: 'inline-block',
+          border: '1px solid #ccc',
+          background: 'white',
+          fontSize: '14px',
+        }}
+        value={value}
+        onChange={(e) => {
+          onChange && onChange(e.target.value);
+        }}
+      >
+        {options.map(({ name, value }) => (
+          <option value={value} key={value}>
+            {name}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
 
