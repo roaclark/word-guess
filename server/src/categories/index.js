e1218cbb317d16dfeb247e23431a1903b3632358
Rachel Rosalia <rachel@rosalia.me>
2020-05-24 19:46:16 -0700

    Add very hard words

--- server/src/categories/index.js
+++ server/src/categories/index.js
@@ -1,9 +1,14 @@
 import words from './words';
+import veryHard from './veryHard';
 
 export default {
   normal: {
     wordList: words,
     label: 'Default',
+  },
+  veryHard: {
+    wordList: veryHard,
+    label: 'Very hard',
   },
   test: {
     wordList: ['test'],
